ac2f117243ce8575f9f68303d91af8996c435706
steadyy847 <94646116+steadyy847@users.noreply.github.com>
2021-11-25 13:13:31 -0600

    Update dateDiff.js

--- test/dateDiff.js
+++ test/dateDiff.js
@@ -33,3 +33,37 @@
 //            return yearDiff + 'Y ' + monthDiff + 'M ' + dayDiff + 'D';
             return yearDiff + (monthDiff/12) + (dayDiff/365);
         } 
+
+				function submitButtonCheck() {
+					var storage = window.localStorage,
+							gender_selected = $("input[name='Gender']:checked").val(),
+							citizenship_selected = $("input[name='Citizenship']:checked").val(),
+							tobacco_selected = $("input[name='Tobacco']:checked").val(),
+							license_selected = $("input[name='License']:checked").val(),
+							asthma_CB = document.getElementById('asthma').checked,
+							depression_CB = document.getElementById('depression').checked,
+							heart_attack_CB = document.getElementById('heart_attack').checked,
+							stroke_CB = document.getElementById('stroke').checked,
+							cancer_CB = document.getElementById('cancer').checked,
+							sleep_apnea_CB = document.getElementById('sleep_apnea').checked,
+							diabetes_CB = document.getElementById('diabetes').checked,
+							no_conditions_CB = document.getElementById('no_conditions').checked;
+
+					// Check that all required fields are complete, & if so, enable the submit button
+					$submit.toggleClass(className,  $dob_month.val() != '' && /^([\w]{1,2})?$/.test($dob_month.val()) &&
+															$dob_day.val() != '' && /^([\w]{1,2})?$/.test($dob_day.val()) &&
+															$dob_year.val() != '' && /^([\w]{4,4})?$/.test($dob_year.val()) &&
+															$zip.val() != '' && /^([\w]{5,5})?$/.test($zip.val()) &&
+															$annual_income.val().length > 5 && $annual_income.val().length < 14 &&
+															$height_feet.val() != '' && /^([\w\']{2,2})?$/.test($height_feet.val()) &&
+															$height_inches.val() != '' && /^([\w\"]{2,3})?$/.test($height_inches.val()) &&
+															$weight.val() != '' && /^([\w]{2,3})?$/.test($weight.val()) &&
+															gender_selected != undefined &&
+															citizenship_selected != undefined &&
+															tobacco_selected != undefined &&
+															license_selected != undefined &&
+															(asthma_CB || depression_CB || heart_attack_CB || stroke_CB || cancer_CB || sleep_apnea_CB || diabetes_CB || no_conditions_CB)
+															);
+					storage.setItem('gender', gender_selected);
+				}
+
