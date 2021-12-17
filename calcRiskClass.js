85a95909e02ef45b4a0f4f5a7fbb0edf70c8e62c
steadyy847 <94646116+steadyy847@users.noreply.github.com>
2021-12-17 17:05:21 -0600

    Create calcRiskClass.js

---
+++ calcRiskClass.js
@@ -0,0 +1,34 @@
+        function calcRiskClass() {
+//            var storage = window.localStorage,
+//                height_feet = document.getElementById("height_feet").value,
+//                height_inches = document.getElementById("height_inches").value,
+//                height_total_inches = (height_feet*12)+height_inches,
+//                weight = document.getElementById("weight").value,
+//                tobacco_selected = $("input[name='Tobacco']:checked").val(),
+//                bmi = Math.round(((weight/height_total_inches)/height_total_inches)*703),
+//                risk_class;
+              alert("In the calcRiskClass function");
+//            alert("BMI="+bmi);
+
+//            if (tobacco_selected = false) {
+//                risk_class = 'nt4';
+//                if (bmi<34) { 
+//                    risk_class = 'nt3';
+//                }
+//                if (bmi<32) { 
+//                    risk_class = 'nt2';
+//                }
+//                if (bmi<30) { 
+//                    risk_class = 'nt1';
+//                }
+//            } else {
+//                if (tobacco_selected = true) {
+//                    risk_class = 'smoke2';
+//                    if (bmi<32) { 
+//                        risk_class = 'smoke1';
+//                    }
+//                }
+//            }
+//            alert("Risk Class="+risk_class);
+//            storage.setItem('risk_class', risk_class);
+        }
