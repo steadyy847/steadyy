685f3633c94a86bdab3e2e89248374ff98b62163
steadyy847 <94646116+steadyy847@users.noreply.github.com>
2021-12-17 17:07:11 -0600

    Update dateDiff.js

--- test/dateDiff.js
+++ test/dateDiff.js
@@ -34,39 +34,3 @@
                 alert("Inside the DateDiff function");
             return yearDiff + (monthDiff/12) + (dayDiff/365);
         }
-
-        function calcRiskClass() {
-//            var storage = window.localStorage,
-//                height_feet = document.getElementById("height_feet").value,
-//                height_inches = document.getElementById("height_inches").value,
-//                height_total_inches = (height_feet*12)+height_inches,
-//                weight = document.getElementById("weight").value,
-//                tobacco_selected = $("input[name='Tobacco']:checked").val(),
-//                bmi = Math.round(((weight/height_total_inches)/height_total_inches)*703),
-//                risk_class;
-
-//            alert("BMI="+bmi);
-              alert("In the calcRiskClass function");
-
-//            if (tobacco_selected = false) {
-//                risk_class = 'nt4';
-//                if (bmi<34) { 
-//                    risk_class = 'nt3';
-//                }
-//                if (bmi<32) { 
-//                    risk_class = 'nt2';
-//                }
-//                if (bmi<30) { 
-//                    risk_class = 'nt1';
-//                }
-//            } else {
-//                if (tobacco_selected = true) {
-//                    risk_class = 'smoke2';
-//                    if (bmi<32) { 
-//                        risk_class = 'smoke1';
-//                    }
-//                }
-//            }
-//            alert("Risk Class="+risk_class);
-//            storage.setItem('risk_class', risk_class);
-        }
