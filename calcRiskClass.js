6b468d2dba4d4aba199a8c4e1b4620e48698e65a
steadyy847 <94646116+steadyy847@users.noreply.github.com>
2021-12-17 23:41:10 -0600

    Update calcRiskClass.js

--- calcRiskClass.js
+++ calcRiskClass.js
@@ -1,41 +1,29 @@
         function calcRiskClass(smoker, height_ft, height_in, weight) {
-//            var storage = window.localStorage,
-//               var weight;
-//                height_feet = document.getElementById("height_feet").value;
-//                height_inches = document.getElementById("height_inches").value,
-//                height_total_inches = (height_feet*12)+height_inches,
-//                weight = document.getElementById("weight").value,
-//                tobacco_selected = $("input[name='Tobacco']:checked").val(),
-//                bmi = Math.round(((weight/height_total_inches)/height_total_inches)*703),
-//                $height_feet = $form.find('input[id="height_feet"]'),
-//                $height_inches = $form.find('input[id="height_inches"]'),
-//                risk_class;
-                   
-              alert("In the calcRiskClass function");
-//                weight = document.getElementById("weight").value;
-//               weight = $weight.val();
-               alert("Weight = "+weight);
-//            alert("BMI="+bmi);
+            var height_total_inches = (height_ft * 12) + height_in,
+                bmi = Math.round(((weight/height_total_inches)/height_total_inches)*703),
+                risk_class;
 
-//            if (tobacco_selected = false) {
-//                risk_class = 'nt4';
-//                if (bmi<34) { 
-//                    risk_class = 'nt3';
-//                }
-//                if (bmi<32) { 
-//                    risk_class = 'nt2';
-//                }
-//                if (bmi<30) { 
-//                    risk_class = 'nt1';
-//                }
-//            } else {
-//                if (tobacco_selected = true) {
-//                    risk_class = 'smoke2';
-//                    if (bmi<32) { 
-//                        risk_class = 'smoke1';
-//                    }
-//                }
-//            }
-//            alert("Risk Class="+risk_class);
-//            storage.setItem('risk_class', risk_class);
+            if (smoker = 'No') {
+                risk_class = 'nt4';
+                if (bmi<34) { 
+                    risk_class = 'nt3';
+                }
+                if (bmi<32) { 
+                    risk_class = 'nt2';
+                }
+                if (bmi<30) { 
+                    risk_class = 'nt1';
+                }
+            } else {
+                if (smoker = 'Yes') {
+                    risk_class = 'smoke2';
+                    if (bmi<32) { 
+                        risk_class = 'smoke1';
+                    }
+                }
+            }
+            return {
+                   bmi,
+                   risk_class
+            };
         }
