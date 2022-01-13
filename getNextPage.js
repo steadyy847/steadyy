e72cd5cb9afb035c1cba6b8c206fdb73f2c7382d
steadyy847 <94646116+steadyy847@users.noreply.github.com>
2022-01-13 12:59:38 -0600

    Update getNextPage.js

--- getNextPage.js
+++ getNextPage.js
@@ -61,3 +61,23 @@ function getNextPage(currentPage) {
       }
    }
 }
+
+function stringToBoolean(string) {
+    switch(string.toLowerCase().trim()){
+        case "true": 
+        case "yes": 
+        case "1": 
+          return true;
+          break;
+            
+        case "false": 
+        case "no": 
+        case "0": 
+        case null: 
+          return false;
+          break;
+
+        default: 
+          return Boolean(string);
+    }
+}
